4dd0dac037a1d2c01b01c789af85991807fa2774
Breno Rios <breno@brenorios.com.br>
2023-10-20 20:39:12 -0300

    Fixed some bugs on Paciente and Data, created toString methods,  and created App class

--- clinica.cpp
+++ clinica.cpp
@@ -1,83 +1,88 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 class Data{
     int dia, mes, ano;
 
     public:
-    Data(int _dia, int _mes, int _ano){
-        this->setData(_dia, _mes, _ano);
-    }
-
-    bool dataValida(int _dia, int _mes, int _ano){
-        int maxDias = 31;
-
-        if((_mes < 1 || _mes > 12) || (_ano < 1900)){
-            return false;
+        Data(int _dia, int _mes, int _ano){
+            this->setData(_dia, _mes, _ano);
         }
 
-        if(_mes == 2){
-            maxDias = 28;
-            if(ano % 4 == 0){
-                maxDias = 29;
+        void setDia(int _dia){
+            if(dataValida(_dia, this->mes, this->ano)){
+                this->dia = _dia;
             }
         }
 
-        if(_mes == 4 || _mes == 6 || _mes == 9 || _mes == 11){
-            maxDias = 30;
+        int getDia(){
+            return this->dia;
         }
 
-        if(_dia < 1 || _dia > maxDias){
-            return false;
+        void setMes(int _mes){
+            if(dataValida(this->dia, _mes, this->ano)){
+                this->mes = _mes;
+            }
         }
 
-        return true;
-    }
+        int getMes(){
+            return this->mes;
+        }
 
-    void setDia(int _dia){
-        if(dataValida(_dia, this->mes, this->ano)){
+        void setAno(int _ano){
+            if(dataValida(this->dia, this->mes, _ano)){
+                this->ano = _ano;
+            }
+        }
+
+        int getAno(){
+            return this->ano;
+        }
+
+        void setData(int _dia, int _mes, int _ano){
+            if(!dataValida(_dia, _mes, _ano)){
+                cout << "Data inválida!" << endl;
+                return;
+            }
+
             this->dia = _dia;
-        }
-    }
-
-    int getDia(){
-        return this->dia;
-    }
-
-    void setMes(int _mes){
-        if(dataValida(this->dia, _mes, this->ano)){
             this->mes = _mes;
-        }
-    }
-
-    int getMes(){
-        return this->mes;
-    }
-
-    void setAno(int _ano){
-        if(dataValida(this->dia, this->mes, _ano)){
             this->ano = _ano;
         }
-    }
 
-    int getAno(){
-        return this->ano;
-    }
-
-    void setData(int _dia, int _mes, int _ano){
-        if(!dataValida){
-            cout << "Data inválida!" << endl;
-            return;
+        string toString(){
+            stringstream ss;
+            ss << this->dia << "/" << this->mes << "/" << this->ano;
+            return ss.str();
         }
 
-        this->dia = _dia;
-        this->mes = _mes;
-        this->ano = _ano;
-    }
+        bool dataValida(int _dia, int _mes, int _ano){
+            int maxDias = 31;
 
-    Data * getData(){
-        return this;
-    }
+            if((_mes < 1 || _mes > 12) || (_ano < 1900)){
+                return false;
+            }
+
+            if(_mes == 2){
+                maxDias = 28;
+                if(ano % 4 == 0){
+                    maxDias = 29;
+                }
+            }
+
+            if(_mes == 4 || _mes == 6 || _mes == 9 || _mes == 11){
+                maxDias = 30;
+            }
+
+            if(_dia < 1 || _dia > maxDias){
+                return false;
+            }
+
+            return true;
+        }
 };
 
 class Paciente{
@@ -91,6 +96,14 @@ class Paciente{
             this->setDtNascimento(_dtNascimento);
         }
 
+        string toString(){
+            stringstream ss;
+            ss << "Cpf: " << this->cpf << endl;
+            ss << "Nome: " << this->nome << endl;
+            ss << "Data de Nascimento: " << this->dtNascimento->toString() << endl;
+            return ss.str();
+        }
+
         void setCpf(string _cpf){
             // fazer validacoes
             this->cpf = _cpf;
@@ -101,7 +114,7 @@ class Paciente{
         }
 
         void setNome(string _nome){
-            this->nome = nome;
+            this->nome = _nome;
         }
 
         string getNome(){
@@ -117,7 +130,48 @@ class Paciente{
         }
 };
 
+class Medico{
+    string crm, nome, especialidade;
+};
+
+class App{
+    vector<Paciente*> pacientes;
+
+    public:
+        void criarPaciente(){
+            int _dia, _mes, _ano;
+            string _cpf, _nome;
+            Data * _data;
+
+            cout << "Digite o cpf do paciente: " << endl;
+            getline(cin >> ws, _cpf);
+
+            cout << "Digite o nome do paciente: " << endl;
+            getline(cin >> ws, _nome);
+
+            cout << "Digite a data de nascimento: " << endl;
+            cin >> _dia >> _mes >> _ano;
+            _data = new Data(_dia, _mes, _ano);
+
+            Paciente * novoPaciente = new Paciente(_cpf, _nome, _data);
+
+            pacientes.push_back(novoPaciente);
+        }
+
+        void imiprimirListaPacientes(){
+            cout << "Lista de Pacientes" << endl;
+            for (auto paciente: this->pacientes) {
+                cout << "-------------------" << endl;
+                cout << paciente->toString() << endl;
+            }
+        }
+};
+
 int main(){
-    
+    App app = App();
+
+    app.criarPaciente();
+    app.imiprimirListaPacientes();
+
     return 0;
 }
