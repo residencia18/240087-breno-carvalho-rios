e3a04aa7acb6f408464c72162a554f95277ff9ec
Breno Rios <breno@brenorios.com.br>
2023-10-21 13:15:01 -0300

    Implemented Paciente menu and some tweaks and validations

--- clinica.cpp
+++ clinica.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 class Data{
@@ -44,7 +45,6 @@ class Data{
 
         void setData(int _dia, int _mes, int _ano){
             if(!dataValida(_dia, _mes, _ano)){
-                cout << "Data inválida!" << endl;
                 return;
             }
 
@@ -59,16 +59,17 @@ class Data{
             return ss.str();
         }
 
-        bool dataValida(int _dia, int _mes, int _ano){
+        static bool dataValida(int _dia, int _mes, int _ano){
             int maxDias = 31;
 
             if((_mes < 1 || _mes > 12) || (_ano < 1900)){
+                cout << "Data inválida!" << endl;
                 return false;
             }
 
             if(_mes == 2){
                 maxDias = 28;
-                if(ano % 4 == 0){
+                if(_ano % 4 == 0){
                     maxDias = 29;
                 }
             }
@@ -78,10 +79,15 @@ class Data{
             }
 
             if(_dia < 1 || _dia > maxDias){
+                cout << "Data inválida!" << endl;
                 return false;
             }
 
             return true;
+        }
+
+        static bool dataValida(Data * _data){
+            return dataValida(_data->getDia(), _data->getMes(), _data->getAno());
         }
 };
 
@@ -100,13 +106,14 @@ class Paciente{
             stringstream ss;
             ss << "Cpf: " << this->cpf << endl;
             ss << "Nome: " << this->nome << endl;
-            ss << "Data de Nascimento: " << this->dtNascimento->toString() << endl;
+            ss << "Data de Nascimento: " << this->dtNascimento->toString();
             return ss.str();
         }
 
         void setCpf(string _cpf){
-            // fazer validacoes
-            this->cpf = _cpf;
+            if(Paciente::cpfValido(_cpf)){
+                this->cpf = _cpf;
+            }
         }
 
         string getCpf(){
@@ -122,11 +129,17 @@ class Paciente{
         }
 
         void setDtNascimento(Data * _dtNascimento){
-            this->dtNascimento = _dtNascimento;
+            if(Data::dataValida(_dtNascimento)){
+                this->dtNascimento = _dtNascimento;
+            }
         }
 
         Data * getDtNascimento(){
             return this->dtNascimento;
+        }
+
+        static bool cpfValido(string _cpf){
+            return regex_match (_cpf, regex("[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}") );
         }
 };
 
@@ -135,10 +148,14 @@ class Medico{
 };
 
 class App{
-    int op = -1;
+    int op;
     vector<Paciente*> pacientes;
 
     public:
+        App(){
+            op = -1;
+        }
+
         void run(){
             while(this->op != 0){
                 lerOpcao();
@@ -150,45 +167,174 @@ class App{
                 mostrarMenu();
                 cout << "Digite uma opção: ";
                 cin >> this->op;
+
+                if(op == 1){
+                    this->criarPaciente();
+                } else if(op == 2){
+                    this->excluirPaciente();
+                } else if(op == 3){
+                    this->alterarPaciente();
+                } else if(op == 4){
+                    this->imiprimirListaPacientes();
+                } else if(op == 5){
+                    this->imiprimirDadosPaciente();
+                } else if(op == 0){
+                    return;
+                } else {
+                    cout << "Opção Inválida" << endl;
+                }
             } while(this->op < 0 || this->op > 5);
+        }
+
+        Data * lerData(){
+            while(true){
+                int _dia, _mes, _ano;
+                cout << "Dia: ";
+                cin >> _dia;
+                cout << "Mes: ";
+                cin >> _mes;
+                cout << "Ano: ";
+                cin >> _ano;
+
+                if(Data::dataValida(_dia, _mes, _ano)){
+                    return new Data(_dia, _mes, _ano);
+                }
+            }
+        }
+
+        string lerCpf(){
+            string _cpf;
+            while(true){
+                cout << "Digite o cpf do paciente: " << endl;
+                getline(cin >> ws, _cpf);
+
+                if(!Paciente::cpfValido(_cpf)){
+                    cout << "CPF inválido!" << endl;
+                } else if(pacienteJaExiste(_cpf)){
+                    cout << "CPF já cadastrado!" << endl;
+                } else{
+                    return _cpf;
+                }
+            }
         }
 
         void mostrarMenu(){
             cout << endl;
             cout << "1. Incluir Paciente" << endl;
-            cout << "2. Excluir Paciente" << endl;
-            cout << "3. Alterar Paciente" << endl;
+            cout << "2. Excluir Paciente (por CPF)" << endl;
+            cout << "3. Alterar Paciente (por CPF)" << endl;
             cout << "4. Listar Pacientes" << endl;
             cout << "5. Localizar Paciente (por CPF)" << endl;
             cout << "0. Sair" << endl;
+            cout << "-------------------" << endl;
         }
 
         void criarPaciente(){
-            int _dia, _mes, _ano;
             string _cpf, _nome;
             Data * _data;
 
-            cout << "Digite o cpf do paciente: " << endl;
-            getline(cin >> ws, _cpf);
+            _cpf = this->lerCpf();
 
             cout << "Digite o nome do paciente: " << endl;
             getline(cin >> ws, _nome);
 
             cout << "Digite a data de nascimento: " << endl;
-            cin >> _dia >> _mes >> _ano;
-            _data = new Data(_dia, _mes, _ano);
+            _data = this->lerData();
 
             Paciente * novoPaciente = new Paciente(_cpf, _nome, _data);
 
             pacientes.push_back(novoPaciente);
         }
 
+        void excluirPaciente(){
+            int index = this->localizarPaciente();
+            if(index == -1){
+                return;
+            }
+
+            this->pacientes.erase(this->pacientes.begin() + index);
+            cout << "Paciente excluído!" << endl;
+        }
+
+        void alterarPaciente(){
+            string _cpf, _nome, _op;
+            int index = this->localizarPaciente();
+
+            if(index == -1){
+                return;
+            }
+
+            Paciente * paciente = this->pacientes.at(index);
+            cout << "-------------------" << endl;
+            cout << "Dados de " << paciente->getNome() << endl;
+            cout << paciente->toString() << endl;
+            cout << "-------------------" << endl;
+            
+            cout << "Deseja alterar o nome? (S/N)" << endl;
+            cin >> _op;
+            if(_op == "S" || _op == "s"){
+                cout << "Digite o novo nome: " << endl;
+                getline(cin >> ws, _nome);
+                paciente->setNome(_nome);
+                cout << "Nome alterado!" << endl;
+            }
+            
+            cout << "Deseja alterar a data de nascimento? (S/N)" << endl;
+            cin >> _op;
+            if(_op == "S" || _op == "s"){
+                cout << "Digite a nova data de nascimento: " << endl;
+                Data * _data  = lerData();
+                paciente->setDtNascimento(_data);
+                cout << "Data de nascimento alterada!" << endl;
+            }
+        }
+
         void imiprimirListaPacientes(){
             cout << "Lista de Pacientes" << endl;
-            for (auto paciente: this->pacientes) {
+            for(auto paciente: this->pacientes) {
                 cout << "-------------------" << endl;
                 cout << paciente->toString() << endl;
             }
+        }
+
+        void imiprimirDadosPaciente(){
+            int pacienteIndex = this->localizarPaciente();
+
+            if(pacienteIndex == -1){
+                return;
+            }
+
+            Paciente * paciente = this->pacientes.at(pacienteIndex);
+            cout << "-------------------" << endl;
+            cout << "Dados de " << paciente->getNome() << endl;
+            cout << paciente->toString() << endl;
+        }
+
+        int localizarPaciente(){
+            string _cpf;
+            cout << "Digite o CPF do paciente:" << endl;
+            getline(cin >> ws, _cpf);
+
+            int index = 0;
+            for(auto paciente: this->pacientes){
+                if(paciente->getCpf() == _cpf){
+                    return index;
+                }
+                index++;
+            }
+            cout << "Paciente não encontrado!" << endl;
+
+            return -1;
+        }
+
+        int pacienteJaExiste(string _cpf){
+            for(auto paciente: this->pacientes){
+                if(paciente->getCpf() == _cpf){
+                    return true;
+                }
+            }
+
+            return false;
         }
 };
 
@@ -196,6 +342,7 @@ int main(){
     App * app = new App();
 
     app->run();
+
 
     return 0;
 }
