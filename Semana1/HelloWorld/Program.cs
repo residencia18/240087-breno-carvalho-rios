43fa525b9250bceff23f2acb8d3d9dc5b54cdd0a
Breno Rios <breno@brenorios.com.br>
2023-11-09 17:11:45 -0300

    Tipos de Dados

--- Semana1/HelloWorld/Program.cs
+++ Semana1/HelloWorld/Program.cs
@@ -1,3 +1,26 @@
 ﻿#region Hello World
     Console.WriteLine("Hello World!");
 #endregion
+
+#region Teste de Tipos de Dados
+    int tipoInteiro;
+    long tipoLong;
+    ulong tipoULong;
+    double tipoDouble;
+    string tipoString;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+    tipoULong = ulong.MaxValue;
+
+    Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
+    Console.WriteLine("O máximo long é: " + tipoLong);
+    Console.WriteLine("O máximo ulong é: " + tipoULong);
+
+    tipoLong = tipoInteiro;
+
+    tipoString = "100";
+    tipoInteiro = int.Parse(tipoString);
+
+    Console.WriteLine(tipoInteiro);
+#endregion
