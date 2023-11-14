530f2fc46533977716916fa3638734ba5dd69f07
Breno Rios <breno@brenorios.com.br>
2023-11-14 16:10:13 -0300

    Adicionado exercicio02

---
+++ Semana2/Execicio02/src/cliente/Cliente.java
@@ -0,0 +1,87 @@
+package cliente;
+import java.util.Scanner;
+
+public class Cliente {
+	String nome, cpf;
+	int idade;
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String nome, cpf;
+		System.out.println("Digite o nome do cliente: ");
+		nome = scanner.nextLine();
+		System.out.println("Digite o cpf do cliente: ");
+		cpf = scanner.nextLine();
+		
+		Cliente novoCliente = new Cliente(nome, cpf);
+		
+		System.out.println("Novo Cliente: ");
+		System.out.println("Nome: " + novoCliente.getNome());
+		System.out.println("CPF: " + novoCliente.getCpf());
+		System.out.println("Idade: " + novoCliente.getIdade());
+		
+		System.out.println("Digite o novo nome (Deixe vazio se não quiser alterar): ");
+		nome = scanner.nextLine();
+		novoCliente.setNome(nome);
+		
+		System.out.println("Digite o novo CPF (Deixe vazio se não quiser alterar): ");
+		cpf = scanner.nextLine();
+		novoCliente.setCpf(cpf);
+		
+		System.out.println("Digite a nova idade (Deixe vazio se não quiser alterar): ");
+		String idadeString = scanner.nextLine();
+		int idade = idadeString.isEmpty() ? 0 : Integer.parseInt(idadeString);
+		novoCliente.setIdade(idade);
+		
+		System.out.println("Novo Cliente: ");
+		System.out.println(novoCliente.toString());
+		
+		scanner.close();
+	}
+	
+	public Cliente(String nome, String cpf){
+		setNome(nome);
+		setCpf(cpf);
+		this.idade = 0;
+	}
+	
+	public String toString() {
+		return "Nome: " + this.nome + "\n" + "CPF: " + this.cpf + "\n" + "Idade: " + this.idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if(nome.isEmpty()) {
+			return;
+		}
+		
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		if(cpf.isEmpty()) {
+			return;
+		}
+		
+		this.cpf = cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		if(idade <= 0) {
+			return;
+		}
+		
+		this.idade = idade;
+	}
+}
