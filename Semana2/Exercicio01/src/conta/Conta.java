214fee72dc6c19216dc05cddcdc7cc3b43b7932c
Breno Rios <breno@brenorios.com.br>
2023-11-14 14:33:47 -0300

    Adicionados getters e setters no exercicio1

--- Semana2/Exercicio01/src/conta/Conta.java
+++ Semana2/Exercicio01/src/conta/Conta.java
@@ -5,5 +5,33 @@ public class Conta {
 	String nome;
 	double saldo;
 	
+	public Conta(int numero, String nome) {
+		this.setNumero(numero);
+		this.setNome(nome);
+		this.setSaldo(0.0);
+	}
 	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 }
