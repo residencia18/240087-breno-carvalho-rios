00924787bcc41d4a4584bdd6f2efd4ec8c5a26a8
Breno Rios <breno@brenorios.com.br>
2023-11-16 14:43:25 -0300

    Adicionado exemplo de laços e condicionais

---
+++ Semana2/Aula01/Program.cs
@@ -0,0 +1,9 @@
+﻿# region Exemplo - Laços e Condicionais
+
+string[] colecao = {"Item 1", "Item 2", "Item 3", "Item 4"};
+
+foreach(string item in colecao) {
+    Console.WriteLine(item);
+}
+
+#endregion
