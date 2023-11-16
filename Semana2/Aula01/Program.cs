5d88c7473180d9caa8abf37b8c561818c2be4e67
Breno Rios <breno@brenorios.com.br>
2023-11-16 15:32:14 -0300

    Adicionados os 3 exercícios da aula 1

--- Semana2/Aula01/Program.cs
+++ Semana2/Aula01/Program.cs
@@ -7,3 +7,41 @@ foreach(string item in colecao) {
 }
 
 #endregion
+
+#region Exercício 1
+
+for(int i = 0; i <= 30; i++){
+    if(i % 3 == 0 || i % 4 == 0){
+        Console.WriteLine(i);
+    }
+}
+
+#endregion
+
+#region Exercício 2
+
+int fibN2 = 0, fibN1 = 1, fibN = fibN2 + fibN1;
+Console.WriteLine(fibN2);
+Console.WriteLine(fibN1);
+
+for(int i = fibN1; fibN <= 100; i++){
+    fibN = fibN1 + fibN2;
+
+    fibN2 = fibN1;
+    fibN1 = fibN;
+
+    Console.WriteLine(fibN);
+}
+
+#endregion
+
+#region Exercício 3
+
+for(int i = 1; i <= 8; i++){
+    for(int j = 1; j <= i; j++){
+        Console.Write(i * j + " ");
+    }
+    Console.WriteLine();
+}
+
+#endregion
