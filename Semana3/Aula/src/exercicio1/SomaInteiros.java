54a9d69b90d6926d2c5c6e366a8b57b23578d654
Breno Rios <breno@brenorios.com.br>
2023-11-21 15:36:53 -0300

    Exercicio 1

---
+++ Semana3/Aula/src/exercicio1/SomaInteiros.java
@@ -0,0 +1,14 @@
+package exercicio1;
+
+public class SomaInteiros {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		
+		System.out.println(somaInteiros(a, b));
+	}
+	
+	public static int somaInteiros(int a, int b) {
+		return a + b;
+	}
+}
