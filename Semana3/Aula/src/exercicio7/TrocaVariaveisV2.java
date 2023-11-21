ce51b890a268aec9ca75f909c471d14c34f4cdd1
Breno Rios <breno@brenorios.com.br>
2023-11-21 15:38:02 -0300

    Exercicio 7

---
+++ Semana3/Aula/src/exercicio7/TrocaVariaveisV2.java
@@ -0,0 +1,20 @@
+package exercicio7;
+
+public class TrocaVariaveisV2 {
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 50;
+		
+		System.out.println("Antes da Troca: ");
+		System.out.println("Var 1 = " + var1);
+		System.out.println("Var 2 = " + var2);
+
+		var1 = var1 ^ var2;
+		var2 = var1 ^ var2;
+		var1 = var1 ^ var2;
+		
+		System.out.println("Depois da Troca: ");
+		System.out.println("Var 1 = " + var1);
+		System.out.println("Var 2 = " + var2);
+	}
+}
