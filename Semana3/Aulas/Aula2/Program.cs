4c9eeea1fec669e8a5218523aaf95747642df355
Breno Rios <breno@brenorios.com.br>
2023-11-23 16:40:24 -0300

    Semana 3 - Aula 2 - Exercicio 2

--- Semana3/Aulas/Aula2/Program.cs
+++ Semana3/Aulas/Aula2/Program.cs
@@ -10,3 +10,22 @@ try {
 }
 
 #endregion
+
+#region Exercicio 2
+
+float divisao(float n1, float n2){
+    if(n2 == 0) {
+        throw new Exception("Não é possível dividir por 0!");
+    }
+
+    return n1 / n2;
+}
+
+try {
+    Console.WriteLine($"{2} / {1} = {divisao(2,1)}");
+    Console.WriteLine($"{2} / {0} = {divisao(2,0)}");
+} catch {
+    Console.WriteLine($"Não é possível dividir por 0");
+}
+
+#endregion
