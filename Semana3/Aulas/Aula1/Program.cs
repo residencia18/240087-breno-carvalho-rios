6df2f3a3a909fdcbf51d98924606460086881a61
Breno Rios <breno@brenorios.com.br>
2023-11-23 15:14:17 -0300

    Semana 3 - Aula 1 - Exercicio 1

---
+++ Semana3/Aulas/Aula1/Program.cs
@@ -0,0 +1,10 @@
+﻿#region Exercicio 1
+
+(string, int) createTuple(string nome, int idade){
+    return (nome, idade);
+}
+
+Console.WriteLine($"{createTuple("Breno", 22)}");
+Console.WriteLine($"{createTuple("João", 27)}");
+
+#endregion
