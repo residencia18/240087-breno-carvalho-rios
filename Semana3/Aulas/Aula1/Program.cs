e98b891fbdec4a56ef211f6fa2cc8c60f88a8a7d
Breno Rios <breno@brenorios.com.br>
2023-11-23 15:16:08 -0300

    Semana 3 - Aula 1 - Exercicio 2

--- Semana3/Aulas/Aula1/Program.cs
+++ Semana3/Aulas/Aula1/Program.cs
@@ -8,3 +8,12 @@ Console.WriteLine($"{createTuple("Breno", 22)}");
 Console.WriteLine($"{createTuple("João", 27)}");
 
 #endregion
+
+#region Exercicio 2
+
+Func<int, int, int> sum = (a, b) => a + b;
+Console.WriteLine($"Soma 22 com 27: {sum(22, 27)}");
+Console.WriteLine($"Soma 18 com 32: {sum(18, 32)}");
+Console.WriteLine($"Soma 9 com 12: {sum(9, 12)}");
+
+#endregion
