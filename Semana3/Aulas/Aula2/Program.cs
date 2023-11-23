6424bd6f7fcf87d21f758c255226ef0a8c739458
Breno Rios <breno@brenorios.com.br>
2023-11-23 16:35:04 -0300

    Semana 3 - Aula 2 - Exercicio 1

---
+++ Semana3/Aulas/Aula2/Program.cs
@@ -0,0 +1,12 @@
+﻿#region Exercicio 1
+
+Console.WriteLine($"Insira um número: ");
+string input = Console.ReadLine() ?? "0";
+int num;
+try {
+    num = int.Parse(input);
+} catch {
+    Console.WriteLine("Por favor digite um número válido");
+}
+
+#endregion
