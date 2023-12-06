d9086e6e73bfedeecb743008f15669cf126390db
Breno Rios <breno@brenorios.com.br>
2023-12-06 16:38:23 -0300

    Adicionado Semana3 - Aula - Exercicio1

---
+++ Semana4/Aula/exercicio0/scripts.js
@@ -0,0 +1,56 @@
+class Tarefa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
+window.addEventListener('load', () => {
+    listaTarefas =  carregaTarefas();
+    for (tarefa of listaTarefas) {
+        adicionaTarefaDOM(tarefa)
+    }
+});
+
+document.getElementById('btn-add-tarefa').addEventListener('click', () => {
+    let tarefa = new Tarefa(document.getElementById('tarefa').value);
+    adicionaTarefaDOM(tarefa);
+    adicionaTarefaNoStorage(tarefa);
+});
+
+function adicionaTarefaDOM(tarefa) {
+    let listaTarefas = document.getElementById('listaTarefas');
+    let tarefaLi = document.createElement('li');
+    let deletar = document.createElement('button');
+    let text = document.createElement('p');
+    text.innerHTML = tarefa.nome;
+    // tarefaLi.innerHTML = tarefa.nome;
+    deletar.innerHTML = "X";
+    deletar.addEventListener('click', () => {
+        document.getElementById("listaTarefas").removeChild(tarefaLi);
+        removeTarefaNoStorage(tarefa);
+    });
+    tarefaLi.appendChild(text);
+    tarefaLi.appendChild(deletar);
+    listaTarefas.appendChild(tarefaLi);
+}
+
+function adicionaTarefaNoStorage(tarefa) {
+    listaTarefas =  carregaTarefas();
+    listaTarefas.push(tarefa);
+    localStorage.setItem("lista-tarefas", JSON.stringify(listaTarefas));
+}
+
+function removeTarefaNoStorage(tarefa) {
+    listaTarefas =  carregaTarefas();
+    listaTarefas.splice(listaTarefas.indexOf(tarefa), 1);
+    localStorage.setItem("lista-tarefas", JSON.stringify(listaTarefas));
+}
+
+function carregaTarefas() {
+    listaTarefas = localStorage.getItem("lista-tarefas");
+    if(listaTarefas == null) {
+        return [];
+    }
+    listaTarefas = JSON.parse(listaTarefas);
+    return listaTarefas;
+}
