4fef2f51f75222d23fbf2deaf54d2484b203cf6a
Breno Rios <breno@brenorios.com.br>
2023-12-05 23:22:33 -0300

    Implementadas classes Cliente e ControleClientes

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,36 @@
 package cliente;
 
 public class Cliente {
-    String cpf, nome;
+    private String cpf, nome;
+
+    public Cliente(String cpf, String nome) throws Exception {
+        this.setCpf(cpf);
+        this.setNome(nome);
+    }
+
+    public String toString() {
+        return String.format("Cpf: %s \nNome: %s", this.cpf, this.nome);
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) throws Exception {
+        if(cpf.length() != 11 || !cpf.matches("[0-9]+")) {
+            throw new Exception("CPF Inválido");
+        }
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) throws Exception {
+        if(nome.isBlank()) {
+            throw new Exception("O nome não pode estar vazio");
+        }
+        this.nome = nome;
+    }
 }
