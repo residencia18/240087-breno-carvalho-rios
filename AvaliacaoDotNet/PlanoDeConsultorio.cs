232f2ba05d988b6cff464102a925c5239114d0f1
Kayque Piton <kayquepiton@gmail.com>
2023-12-07 15:25:50 -0300

    Atualizar o PlanoDeConsultorio.cs

--- AvaliacaoDotNet/PlanoDeConsultorio.cs
+++ AvaliacaoDotNet/PlanoDeConsultorio.cs
@@ -1,7 +1,16 @@
 namespace AvaliacaoDotNet
+using System.Collections.Generic;
+
+public class PlanoDeConsultorio
 {
-    public class PlanoDeConsultorio
+    public string Titulo { get; }
+    public double ValorPorMes { get; }
+    public List<string> Beneficios { get; }
+
+    public PlanoDeConsultorio(string titulo, double valorPorMes, List<string> beneficios)
     {
-        
+        Titulo = titulo;
+        ValorPorMes = valorPorMes;
+        Beneficios = beneficios;
     }
 }
