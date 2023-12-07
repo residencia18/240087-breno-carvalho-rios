673ab2cde23cd76f21a6a5c7a8ee2ee39c1ef37d
EzekLobo <eloliveira.cic@uesc.br>
2023-12-07 01:34:27 -0300

    Pagamento

--- src/financeiro/Pagamento.java
+++ src/financeiro/Pagamento.java
@@ -1,9 +1,28 @@
 package financeiro;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pagamento {
-    LocalDateTime data;
-    double valor;
-    Fatura fatura;
+     private Fatura fatura;
+    private double valor;
+    private LocalDateTime data;
+    private List<Reembolso> reembolsos;
+
+    public Pagamento(Fatura fatura, double valor, LocalDateTime data) {
+        this.fatura = fatura;
+        this.valor = valor;
+        this.data = data;
+        this.reembolsos = new ArrayList<>();
+    }
+
+    public void incluirReembolso(Reembolso reembolso) {
+        this.reembolsos.add(reembolso);
+    }
+
+    public List<Reembolso> listarReembolsos() {
+        return this.reembolsos;
+    }
+
 }
