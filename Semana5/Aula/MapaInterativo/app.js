12016d37cccda490906d19c8f410023b17111cc1
Breno Rios <breno@brenorios.com.br>
2023-12-08 12:12:15 -0300

    Semana5 - Aula - Exercicio1 - Atualizado

--- Semana5/Aula/MapaInterativo/app.js
+++ Semana5/Aula/MapaInterativo/app.js
@@ -1,7 +1,8 @@
 fetch('https://restcountries.com/v3.1/all')
     .then(function (resposta) { return resposta.json(); })
-    .then(function (paises) { return paises.map(function (pais) { return {name: pais.name.common, latlng: pais.latlng}; }); })
+    .then(function (paises) { return paises.map(function (pais) { return {name: pais.name.common, latlng: pais.latlng}})})
     .then(function (paises) {
+      paises.sort((a, b) => a.name.localeCompare(b.name));
       for (pais of paises) {
         let opt = document.createElement("option");
         opt.value = pais.latlng;
@@ -12,6 +13,7 @@ fetch('https://restcountries.com/v3.1/all')
     .catch(function (error) { return console.error('Erro com a chamada fetch: ', error); });
 
 var map = L.map('map').setView([-14.794851092521004, -39.25978803993592], 4);
+
 document.getElementById("paises").addEventListener('change', (event) => {
   let params = event.target.value.split(',');
   map.setView(params, 4);
