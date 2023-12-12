e6484643a1030bf2b903c67281043790ad716965
Breno Rios <breno@brenorios.com.br>
2023-12-12 08:27:59 -0300

    Corrigida api request com http

--- Semana5/Pratica005/script.js
+++ Semana5/Pratica005/script.js
@@ -29,7 +29,7 @@ async function getWeather(coordParam) {
         if(coordParam) {
             coords = coordParam;
         }
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${coords.lat},${coords.lon}&aqi=no&lang=pt`);
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${coords.lat},${coords.lon}&aqi=no&lang=pt`);
         const json = await response.json();
         return json;
     } catch (error) {
