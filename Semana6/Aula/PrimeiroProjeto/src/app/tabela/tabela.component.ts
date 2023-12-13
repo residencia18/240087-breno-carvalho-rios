6134c4667a08f4ad769af965ee7547e08e6e6d32
Breno Rios <breno@brenorios.com.br>
2023-12-13 15:55:07 -0300

    Adicionada Semana6 - Aula

---
+++ Semana6/Aula/PrimeiroProjeto/src/app/tabela/tabela.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-tabela',
+    templateUrl: 'tabela.component.html',
+    styleUrls: ['tabela.component.css']
+})
+
+export class TabelaComponent {}
