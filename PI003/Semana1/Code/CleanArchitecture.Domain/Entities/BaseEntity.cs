befcd97f4e2b7aa1feb9a0f2660ab84f901b9656
EzekLobo <eloliveira.cic@uesc.br>
2024-01-07 18:52:27 -0300

    Organização das camadas

---
+++ PI003/Semana1/Code/CleanArchitecture.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+
+
+namespace CleanArchitecture.Domain.Entities;
+    public abstract class BaseEntity{
+        public Guid Id { get; set;}
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateUpdated { get; set; }
+        public DateTimeOffset DateDelected { get; set;}
+
+
+    }
---
+++ PI003/Semana1/Code/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,5 @@
+namespace CleanArchitecture.Domain.Entities;
+    public sealed class User : BaseEntity{
+        public string? Email {get; set;}
+        public string? Name { get; set;}
+    }
