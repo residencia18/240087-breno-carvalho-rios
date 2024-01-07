1d3e02e000ba5237c46ffff611d3c315389feddf
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-01-07 17:26:21 -0300

    Atualização PI--2

--- PI002/Semana1/MinimalAPI/Program.cs
+++ PI002/Semana1/MinimalAPI/Program.cs
@@ -3,7 +3,7 @@ using Semana1.MinimalAPI;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/daniel/", () => Daniel.View());
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/daniel", () => Daniel.View());
 
 app.Run();
