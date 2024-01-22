fd1c3b9427b81c74a477065a91cb1b14a3af9062
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-01-22 20:37:32 -0300

    feat:criação do entidade usuario e migrations

--- ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
+++ ResTIConnect/ResTIConnect.Infrastructure/Context/ResTIConnectContext.cs
@@ -9,10 +9,11 @@ public class ResTIConnectContext : DbContext
     public DbSet<Log> Logs { get; set; }
     public DbSet<Endereco> Enderecos { get; set; }
     public DbSet<Perfil> Perfis { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        var connectionString = "server=localhost;user=root;password=12345;database=resticonnect";
+        var connectionString = "server=localhost;user=root;password=88652927;database=resticonnect";
         var serverVersion = ServerVersion.AutoDetect(connectionString);
         
         optionsBuilder.UseMySql(connectionString, serverVersion);
@@ -26,6 +27,13 @@ public class ResTIConnectContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Endereco>().ToTable("Enderecos").HasKey(m => m.EnderecoId);
 
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Perfil>().ToTable("Perfis").HasKey(p => p.PerfilId);
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Usuario>().ToTable("Usuarios").HasKey(u => u.UsuarioId);     
+            
+            
        
     }
 }
