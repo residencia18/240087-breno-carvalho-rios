93c6682b3cc5a50e0f2951b2a246847af0b85e0b
Alan Santos <30904884+AlanSantos01@users.noreply.github.com>
2024-01-22 21:29:40 -0300

    Update 01_create_servico.sql

--- ProjetoOrdemServico/files/database/01_create_servico.sql
+++ ProjetoOrdemServico/files/database/01_create_servico.sql
@@ -1,7 +1,7 @@
-CREATE TABLE servico (
-servico_id INT PRIMARY KEY,
-nome VARCHAR(255) NOT NULL,
-descricao TEXT,
-preco DECIMAL(10, 2) NOT NULL,
-tipo_servico VARCHAR(50)
+CREATE TABLE `osmanagerdb`.`servico` (
+`nome` VARCHAR(100) NOT NULL,
+`descricao` TEXT,
+`preco` DECIMAL(10, 2) NOT NULL,
+`tipo_servico` VARCHAR(50)
+PRIMARY KEY (`servico_id`)
 );
