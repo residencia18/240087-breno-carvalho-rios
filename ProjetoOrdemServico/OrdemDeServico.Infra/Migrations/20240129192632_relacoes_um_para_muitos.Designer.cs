9c945a7950fdf70d10e4fd6a3082af2486bce3c5
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-01-29 16:29:10 -0300

    Migrations de relacionamento um para muitos

--- ProjetoOrdemServico/OrdemDeServico.Infra/Migrations/20240129192632_relacoes_um_para_muitos.Designer.cs
+++ ProjetoOrdemServico/OrdemDeServico.Infra/Migrations/20240129192632_relacoes_um_para_muitos.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResTIConnect.Infrastructure;
 
@@ -10,9 +11,11 @@ using ResTIConnect.Infrastructure;
 namespace OrdemDeServico.Infra.Migrations
 {
     [DbContext(typeof(OrdemDeServicoContext))]
-    partial class OrdemDeServicoContextModelSnapshot : ModelSnapshot
+    [Migration("20240129192632_relacoes_um_para_muitos")]
+    partial class relacoes_um_para_muitos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
