b4f142e26cd0fa589513681073c99f8f72f9a7be
Breno Rios <breno@brenorios.com.br>
2024-01-29 23:46:29 -0300

    Add CRUD, Fixes

--- ProjetoOrdemServico/OrdemDeServico.Infra/Migrations/20240130022634_fix_pagamento.Designer.cs
+++ ProjetoOrdemServico/OrdemDeServico.Infra/Migrations/20240130022634_fix_pagamento.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResTIConnect.Infrastructure;
 
@@ -10,9 +11,11 @@ using ResTIConnect.Infrastructure;
 namespace OrdemDeServico.Infra.Migrations
 {
     [DbContext(typeof(OrdemDeServicoContext))]
-    partial class OrdemDeServicoContextModelSnapshot : ModelSnapshot
+    [Migration("20240130022634_fix_pagamento")]
+    partial class fix_pagamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjetoOrdemServico/OrdemDeServico.Infra/Migrations/20240130022634_fix_pagamento.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrdemDeServico.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_pagamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pagamento_Cliente_ClienteId",
+                table: "Pagamento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pagamento_ClienteId",
+                table: "Pagamento");
+
+            migrationBuilder.DropColumn(
+                name: "ClienteId",
+                table: "Pagamento");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClienteId",
+                table: "Pagamento",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pagamento_ClienteId",
+                table: "Pagamento",
+                column: "ClienteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pagamento_Cliente_ClienteId",
+                table: "Pagamento",
+                column: "ClienteId",
+                principalTable: "Cliente",
+                principalColumn: "ClienteId");
+        }
+    }
+}
