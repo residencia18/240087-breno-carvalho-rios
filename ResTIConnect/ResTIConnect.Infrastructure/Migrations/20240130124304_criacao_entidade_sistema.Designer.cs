5684fcee977b7e038d15a8914f662f167470d0ff
Breno Rios <breno@brenorios.com.br>
2024-01-30 09:44:20 -0300

    Criada migration da entidade Servico

--- ResTIConnect/ResTIConnect.Infrastructure/Migrations/20240130124304_criacao_entidade_sistema.Designer.cs
+++ ResTIConnect/ResTIConnect.Infrastructure/Migrations/20240130124304_criacao_entidade_sistema.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResTIConnect.Infrastructure;
 
@@ -10,9 +11,11 @@ using ResTIConnect.Infrastructure;
 namespace ResTIConnect.Infrastructure.Migrations
 {
     [DbContext(typeof(ResTIConnectContext))]
-    partial class ResTIConnectContextModelSnapshot : ModelSnapshot
+    [Migration("20240130124304_criacao_entidade_sistema")]
+    partial class criacao_entidade_sistema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
