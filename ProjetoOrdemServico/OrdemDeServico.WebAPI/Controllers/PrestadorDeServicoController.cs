52beb620a29d42540fd38a57f05a51c36f469093
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-02-05 22:21:00 -0300

    Implementação do controller de Prestador de Serviço em OrdemDeServico.WebAPI

---
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/Controllers/PrestadorDeServicoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdemDeServico.WebAPI.InputModels.NewPrestadorDeServicoInputModel;
+
+namespace OrdemDeServico.WebAPI.Controllers.PrestadorDeServico;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class PrestadorDeServicoController : ControllerBase
+{
+    [HttpGet("prestadores-de-servico")]
+    public IActionResult Get()
+    {
+        return NoContent();
+    }
+
+    [HttpGet("prestador-de-servico/{id}")]
+    public IActionResult GetById(int id)
+    {
+        return NoContent();
+    }
+
+    [HttpPost("prestador-de-servico")]
+    public IActionResult Post([FromBody] NewPrestadorDeServicoInputModel prestadorDeServico)
+    {
+        return NoContent();
+    }
+
+    [HttpPut("prestador-de-servico/{id}")]
+    public IActionResult Put(int id, [FromBody] NewPrestadorDeServicoInputModel prestadorDeServico)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("prestador-de-servico/{id}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+}
---
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewPrestadorDeServicoInputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdemDeServico.WebAPI.InputModels.NewPrestadorDeServicoInputModel;
+
+public class NewPrestadorDeServicoInputModel
+{
+    public int PrestadorDeServicoId { get; set; }
+    public required string Nome { get; set; }
+    public required string Especialidade { get; set; }
+    public required string Telefone { get; set; }
+    public required NewEnderecoInputModel Endereco { get; set; }
+}
