7935a160808bbe4892b9f19154909725a93d60da
Alan Carlos <alancarlospereira.ti@gmail.com>
2024-02-06 10:37:46 -0300

    Implementação do controller de Pagamento

--- ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewPagamentoInputModel.cs
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewPagamentoInputModel.cs
@@ -4,5 +4,5 @@ public class NewPagamentoInputModel
     public required double Valor { get; set; }
     public required DateTime Data_pagamento { get; set; }
     public required string Metodo_pagamento { get; set; }
-    public required NewOrdemDeServicoInputModel Ordem_de_servico { get; set; }
+    public required int OrdemServicoId { get; set; }
 }
