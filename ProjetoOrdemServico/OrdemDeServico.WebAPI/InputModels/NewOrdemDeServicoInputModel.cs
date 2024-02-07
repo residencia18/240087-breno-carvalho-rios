e0277fe2e8bb7694f406f989f9dd06511f8b50d0
EzekLobo <eloliveira.cic@uesc.br>
2024-02-07 14:41:48 -0300

    Update NewOrdemDeServicoInputModel properties

--- ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewOrdemDeServicoInputModel.cs
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewOrdemDeServicoInputModel.cs
@@ -4,6 +4,7 @@ public class NewOrdemDeServicoInputModel
 {
     public required string Descricao { get; set; }
     public required DateTime Data { get; set; }
-    public required NewPrestadorDeServicoInputModel PrestadorDeServico { get; set; }
+    public required int PrestadorDeServico { get; set; }
+    public required int Cliente { get; set; }
     public required string Status { get; set; }
 }
