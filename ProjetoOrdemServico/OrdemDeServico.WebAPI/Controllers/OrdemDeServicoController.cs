b74055ae6df4fba8d9e914d86e1c8863bca77313
EzekLobo <eloliveira.cic@uesc.br>
2024-02-07 00:27:53 -0300

    Add OrdemDeServicoController and NewOrdemDeServicoInputModel class

---
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/Controllers/OrdemDeServicoController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using OrdemDeServico.WebAPI.InputModels;
+
+namespace OrdemDeServico.WebAPI.Controllers.OrdemDeServico;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class OrdemDeServicoController : ControllerBase
+{
+    [HttpGet("ordens-de-servico")]
+    public IActionResult Get()
+    {
+        return NoContent();
+    }
+
+    [HttpGet("ordem-de-servico/{id}")]
+    public IActionResult GetById(int id)
+    {
+        return NoContent();
+    }
+
+    [HttpPost("ordem-de-servico")]
+    public IActionResult Post([FromBody] NewOrdemDeServicoInputModel ordemDeServico)
+    {
+        return NoContent();
+    }
+
+    [HttpPut("ordem-de-servico/{id}")]
+    public IActionResult Put(int id, [FromBody] NewOrdemDeServicoInputModel ordemDeServico)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("ordem-de-servico/{id}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+}
--- ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewOrdemDeServicoInputModel.cs
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/InputModels/NewOrdemDeServicoInputModel.cs
@@ -1,5 +1,9 @@
 namespace OrdemDeServico.WebAPI.InputModels;
+
 public class NewOrdemDeServicoInputModel
 {
-
+    public required string Descricao { get; set; }
+    public required DateTime Data { get; set; }
+    public required NewPrestadorDeServicoInputModel PrestadorDeServico { get; set; }
+    public required string Status { get; set; }
 }
