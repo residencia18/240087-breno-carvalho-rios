c07c41298ed1fb736881b81431e811398e194c0e
EzekLobo <eloliveira.cic@uesc.br>
2024-02-07 19:20:26 -0300

    Add package references and configure DbContext in TechMed.Infrastructure and TechMed.WebAPI projects

--- TechMed/TechMed.WebAPI/Program.cs
+++ TechMed/TechMed.WebAPI/Program.cs
@@ -1,20 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using TechMed.Infrastructure.Persistence;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<TechMedDbContext>(options => {
+    var connectionString = builder.Configuration.GetConnectionString("TechMedDb");
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+      options.UseMySql(connectionString, serverVersion);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
