dfc02ed2536c766ebd0b23cfd46a4ba3c0d5ed53
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-02-12 00:15:07 -0300

    Correção Endpoints do CRUD da entidade Endereco

--- ResTIConnect/ResTIConnect.Application/Services/Interfaces/IEnderecoService.cs
+++ ResTIConnect/ResTIConnect.Application/Services/Interfaces/IEnderecoService.cs
@@ -7,7 +7,6 @@ public interface IEnderecoService
 {
     public List<EnderecoViewModel> GetAll();
     public EnderecoViewModel? GetById(int id);
-    public List<EnderecoViewModel> GetByUserId(int userId);
     public int Create(NewEnderecoInputModel endereco);
     public void Update(int id, NewEnderecoInputModel endereco);
     public void Delete(int id);
