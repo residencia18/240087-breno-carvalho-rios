ac626d365c10a37c456376714addbaecdac8d227
Marcelo da Silva Cruz <mcruz@uesc.br>
2024-02-13 13:29:37 -0300

    Criação de Servico Ordem de Servico endpoint

---
+++ ProjetoOrdemServico/OrdemDeServico.Application/InputModels/NewServicoOrdemServicoInputModel.cs
@@ -0,0 +1,8 @@
+namespace OrdemDeServico.Application.InputModels;
+public class NewServicoOrdemServicoInputModel
+{
+    public int ServicoId { get; set; }
+    public int OrdemServicoId { get; set; }
+    public int EnderecoId { get; set; }
+
+}
