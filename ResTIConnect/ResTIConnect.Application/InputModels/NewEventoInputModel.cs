e88bf90ff302ab68a8662018cddec97aa4574de6
EzekLobo <eloliveira.cic@uesc.br>
2024-02-15 01:05:36 -0300

    Update NewEventoInputModel properties to be required

--- ResTIConnect/ResTIConnect.Application/InputModels/NewEventoInputModel.cs
+++ ResTIConnect/ResTIConnect.Application/InputModels/NewEventoInputModel.cs
@@ -2,10 +2,10 @@ namespace ResTIConnect.Application.InputModels
 {
     public class NewEventoInputModel
     {
-        public string? Tipo { get; set; }
+        public required string Tipo { get; set; }
         public string? Descricao { get; set; }
-        public string? Codigo { get; set; }
-        public string? Conteudo { get; set; }
+        public required string Codigo { get; set; }
+        public required string Conteudo { get; set; }
         public DateTimeOffset DataHoraOcorrencia { get; set; }
     }
 }
