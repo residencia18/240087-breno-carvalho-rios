a93b359f3ccb5131d4f450d039a1e5dc7523fb38
Alan Carlos <alancarlospereira.ti@gmail.com>
2024-02-15 10:59:49 -0300

    att

--- ResTIConnect/ResTIConnect.Application/ViewModels/SistemaViewModel.cs
+++ ResTIConnect/ResTIConnect.Application/ViewModels/SistemaViewModel.cs
@@ -4,9 +4,9 @@ namespace ResTIConnect.Application.ViewModels
     public class SistemaViewModel
     {
         public int SistemaId { get; set; }
-        public required string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public string? Tipo { get; set; }
         public List<UsuarioViewModel>? Users { get; set; } = new List<UsuarioViewModel>();
-        //public List<EventoViewModel>? Eventos { get; set; } = new List<EventoViewModel>();
+        public List<EventoViewModel>? Eventos { get; set; } = new List<EventoViewModel>();
     }
 }
