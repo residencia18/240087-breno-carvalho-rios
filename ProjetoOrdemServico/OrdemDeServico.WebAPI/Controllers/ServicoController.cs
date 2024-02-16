8b12c30c434d1eb7ca44dfacdc23d3851597f833
EzekLobo <eloliveira.cic@uesc.br>
2024-02-16 08:22:47 -0300

    Refactor ServicoController to use ServicoService

--- ProjetoOrdemServico/OrdemDeServico.WebAPI/Controllers/ServicoController.cs
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/Controllers/ServicoController.cs
@@ -1,44 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using OrdemDeServico.Application.InputModels;
+using OrdemDeServico.Application.Services.Interfaces;
+using OrdemDeServico.Application.ViewModels;
 
-namespace OrdemDeServico.WebAPI.Controllers
+namespace OrdemServico.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/v0.1/")]
+    [Route("/api/v0.1/")]
     public class ServicoController : ControllerBase
     {
+        private readonly IServicoService _servicoService;
+
+        public List<ServicoViewModel> Servicos => _servicoService.GetAll().ToList();
+
+        public ServicoController(IServicoService servicoService) => _servicoService = servicoService;
+        
+
         [HttpGet("servicos")]
         public IActionResult Get()
         {
-
-            return NoContent();
+            var servicos = _servicoService.GetAll();
+            return Ok(servicos);
         }
 
         [HttpGet("servico/{id}")]
         public IActionResult GetById(int id)
         {
-
-            return NoContent();
+            var servico = _servicoService.GetById(id);
+            if (servico == null)
+            {
+                return NotFound();
+            }
+            return Ok(servico);
         }
 
         [HttpPost("servico")]
-        public IActionResult Post([FromBody] NewServicoInputModel servico)
+        public IActionResult Post([FromBody] NewServicoInputModel servicoInputModel)
         {
-
-            return NoContent();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var servicoId = _servicoService.Create(servicoInputModel);
+            return CreatedAtAction(nameof(GetById), new { id = servicoId }, servicoInputModel);
         }
 
         [HttpPut("servico/{id}")]
-        public IActionResult Put(int id, [FromBody] NewServicoInputModel servico)
+        public IActionResult Put(int id, [FromBody] NewServicoInputModel servicoInputModel)
         {
-
-            return NoContent();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _servicoService.Update(id, servicoInputModel);
+            return Ok();
         }
 
         [HttpDelete("servico/{id}")]
         public IActionResult Delete(int id)
         {
-
+            _servicoService.Delete(id);
             return NoContent();
         }
     }
