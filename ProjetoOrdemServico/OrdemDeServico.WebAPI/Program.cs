a5ce9ecb72ac92e89585aa88977bf6bebf6c694f
Daniel Coutinho Neto <danielcoutinhoneto@outlook.com>
2024-02-28 23:19:28 -0300

    Implementando autenticação via Jwt a partir do Login.

--- ProjetoOrdemServico/OrdemDeServico.WebAPI/Program.cs
+++ ProjetoOrdemServico/OrdemDeServico.WebAPI/Program.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using OrdemDeServico.Application.Services;
 using OrdemDeServico.Application.Services.Interfaces;
 using OrdemDeServico.Infra;
@@ -24,6 +27,22 @@ builder.Services.AddDbContext<OrdemDeServicoContext>(options =>
     options.UseMySql(connectionString, serverVersion);
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
+        };
+    });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -40,6 +59,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
