9cab5b7fa4ac3cb54e1777d55ca07fa88707ed0c
Breno Rios <breno@brenorios.com.br>
2024-03-14 22:55:55 -0300

    Add Semana01/Aula02/Pratica03-Parcial

---
+++ Praticas/Exercicio03/Program.cs
@@ -0,0 +1,16 @@
+﻿enum Exercicio
+{
+    Academia,
+    Luta,
+    Corrida
+};
+
+public class App
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Texto do exercício {Exercicio.Luta}");
+        Console.WriteLine($"Texto do exercício {Exercicio.Corrida}");
+        Console.WriteLine($"Texto do exercício {Exercicio.Academia}");
+    }
+}
