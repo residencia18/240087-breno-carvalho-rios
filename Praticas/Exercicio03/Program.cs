953abb613f9b52929ddfd0712f753352c8580d6e
Breno Rios <breno@brenorios.com.br>
2024-03-15 11:09:33 -0300

    Finalizada Semana01/Aula02/Pratica03

--- Praticas/Exercicio03/Program.cs
+++ Praticas/Exercicio03/Program.cs
@@ -1,16 +1,32 @@
 ﻿enum Exercicio
 {
-    Academia,
-    Luta,
-    Corrida
+    Academia = 1,
+    Luta = 2,
+    Corrida = 3
 };
 
 public class App
 {
     public static void Main()
     {
-        Console.WriteLine($"Texto do exercício {Exercicio.Luta}");
-        Console.WriteLine($"Texto do exercício {Exercicio.Corrida}");
-        Console.WriteLine($"Texto do exercício {Exercicio.Academia}");
+        Console.WriteLine($"Opções de exercício:");
+
+        int i = 1;
+        foreach (var exercicio in Enum.GetValues<Exercicio>())
+        {
+            Console.WriteLine($"{i}. {exercicio}");
+            i++;
+        }
+
+
+        try
+        {
+            int opcao = int.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine($"Exercício escolhido: {Enum.GetName(typeof(Exercicio), opcao)}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"É necessário informar um número valido");
+        }
     }
 }
