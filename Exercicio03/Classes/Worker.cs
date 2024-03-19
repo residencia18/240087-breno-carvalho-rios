4c7bc4a16587e0bd9c7109d85b6fe2305bb91e5f
Breno Rios <breno@brenorios.com.br>
2024-03-19 19:58:47 -0300

    Add Exercicio03

---
+++ Exercicio03/Classes/Worker.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio03.Classes;
+
+class Worker
+{
+    public string NomeWorker { get; set; }
+
+    public Worker(string nomeWorker)
+    {
+        NomeWorker = nomeWorker;
+    }
+    public void Work()
+    {
+        Random rand = new Random();
+        for (int i = 1; i <= 10; i++)
+        {
+            Thread.Sleep(rand.Next(500, 3000));
+            Console.WriteLine($"{NomeWorker} - Passo {i}");
+        }
+    }
+}
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,10 @@
+﻿using Exercicio03.Classes;
+
+Worker worker1 = new Worker("Worker 1");
+Worker worker2 = new Worker("Worker 2");
+Thread thread1 = new Thread(worker1.Work);
+Thread thread2 = new Thread(worker2.Work);
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
