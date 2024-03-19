cd6ca9ee0383ad747949ef2690f6b069e3c07c53
Breno Rios <breno@brenorios.com.br>
2024-03-19 20:03:06 -0300

    Add Exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,15 @@
+﻿var task1 = Task.Run(async () => await DoWorkAsync("Tarefa 1", 5, 500));
+var task2 = Task.Run(async () => await DoWorkAsync("Tarefa 2", 7, 1000));
+
+await Task.WhenAll(task1, task2);
+
+Console.WriteLine("As duas tarefas foram concluídas.");
+
+static async Task DoWorkAsync(string nome, int numeroIteracoes, int tempoEspera)
+{
+    for (int i = 1; i <= numeroIteracoes; i++)
+    {
+        await Task.Delay(TimeSpan.FromMilliseconds(tempoEspera));
+        Console.WriteLine($"{nome} - Iteração {i}/{numeroIteracoes}");
+    }
+}
